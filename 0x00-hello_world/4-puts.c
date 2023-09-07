2837dbe081ef787f560afb03d7f6a2b8e3c22d03
assma-codes <assma.hm89@gmail.com>
2023-09-07 12:01:19 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - this is the main function
- * Return : value of 0
+ * main - main block
+ * Return: 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
