51ea2e3f41b9ce85bed4a18979c87d1d2eee9d06
assma-codes <assma.hm89@gmail.com>
2023-09-08 19:43:04 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - this is the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
