aaada884bb70030715dc4bdd642cb4a5cc1020bd
assma-codes <assma.hm89@gmail.com>
2023-09-08 20:26:46 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - this is the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
