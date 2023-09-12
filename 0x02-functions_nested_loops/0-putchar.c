dd60e1dd2b2d9b1af55ced7e6e030f82c3f3b74d
assma-codes <assma.hm89@gmail.com>
2023-09-12 11:43:43 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - this is the main function call _putchar 
+ * return : 0
+ */
+int main(void)
+{
+	-Putchar('_');
+	-Putchar('p');
+	-Putchar('u');
+	-Putchar('t');
+	-Putchar('c');
+	-Putchar('h');
+	-Putchar('a');
+	-Putchar('r');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is the readme file at the root of the directory 0x02-functions_nested_loops
