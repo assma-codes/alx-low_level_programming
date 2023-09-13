e4805ca3bbc14e0dac3ebbed36a767f52068e9b1
assma-codes <assma.hm89@gmail.com>
2023-09-12 21:51:22 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
-*_islower - checks for lowercase character.
-*Return: return 0 otherwise
+*_islower - checks if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
 */
 int _islower(int c)
 {
