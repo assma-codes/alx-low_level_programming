b8cc909facec983906666373c73d132eb0490718
assma-codes <assma.hm89@gmail.com>
2023-09-20 10:12:46 -0700

    task11

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
-	
+
 	a[2] = 1024;
 	p = &n;
 	/*
@@ -15,7 +20,7 @@ int main(void)
 	 * * - you are not allowed to modify p
 	 * * - only one statement
 	 * * - you are not allowed to code anything else than this line of code
-	 * */
+	 */
 	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
