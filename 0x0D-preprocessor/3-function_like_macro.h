e78c9f73b9a6c78406cc9c3a9eada0579470318e
assma-codes <assma.hm89@gmail.com>
2023-10-10 13:00:12 -0700

    task3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,9 @@
-#ifndef MACRO_HEADER
-#define MACRO_HEADER
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
 /**
- * File: 3-function_like_macro.hi
- *
- * Desc: haeder file
- * X: variable for which get the absolute value
+ * File: 3-function_like_macro
+ * @X: input
+ * Desc: header file
  */
 #define ABS(X) ((X) < 0 ? -(1) * (X) : (X))
 
