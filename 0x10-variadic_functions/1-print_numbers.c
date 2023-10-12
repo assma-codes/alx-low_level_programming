da1c4051905890c06840779a207d1d78ab263614
assma-codes <assma.hm89@gmail.com>
2023-10-12 10:30:07 -0700

    task1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - print numbers seperated by comma
+ * @n: number of intigers
+ * Return: no return
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list_ptr;
+	unsigned int i;
+
+	va_start(list_ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list_ptr, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(list_ptr);
+
+}
