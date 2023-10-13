56a6a614e1782675a22d33eef92a9edfebbc3917
assma-codes <assma.hm89@gmail.com>
2023-10-12 21:03:19 -0700

    task1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,13 +19,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(list_ptr, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(list_ptr, char*);
-	if (str != NULL)
-		printf("%s", str);
-	else
-		printf("(nil)");
-	if (i < (n-1)&& separator != NULL)
-		printf("%s", separator);
+		str = va_arg(list_ptr, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (i < (n-1)&& separator != NULL)
+			printf("%s", separator);
 	}
 	va_end(list_ptr);
 	printf("\n");
