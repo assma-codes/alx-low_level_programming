82853f7485248a4f96a67041ff18c275c7549a0c
assma-codes <assma.hm89@gmail.com>
2023-10-13 07:48:21 -0700

    task2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,30 +3,34 @@
 #include <stdarg.h>
 
 /**
- * print_strings -  print only strings
- *@n: number of arguments
- *@separator: comma to be printed between strings
+ * print_strings - prints strings.
+ * @separator: string to be printed between the strings.
+ * @n: number of strings passed to the function.
  *
- *Return: no return while it is void function
+ * Return: no return.
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list valist;
 	unsigned int i;
-	va_list list_ptr;
 	char *str;
 
-	va_start(list_ptr, n);
+	va_start(valist, n);
+
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(list_ptr, char *);
+		str = va_arg(valist, char *);
+
 		if (str)
 			printf("%s", str);
 		else
 			printf("(nil)");
-		if (i < (n-1)&& separator != NULL)
-			printf("%s", separator);
+
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
 	}
-	va_end(list_ptr);
+
 	printf("\n");
+	va_end(valist);
 }
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings -  print only strings
+ *@n: number of arguments
+ *@separator: comma to be printed between strings
+ *
+ *Return: no return while it is void function
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i;
+	va_list list_ptr;
+	char *str;
+/*
+	va_start(list_ptr, n);
+	va_end(list_ptr);
+	printf("\n");
+*/
+	}
