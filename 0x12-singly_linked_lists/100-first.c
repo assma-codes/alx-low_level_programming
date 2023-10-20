ba9907ad2d1abf6c328249d01173ec06deddd4d7
assma-codes <assma.hm89@gmail.com>
2023-10-20 14:12:55 -0700

    statement

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_first - funcion that prints a statement string before the main,
+ * function in executed.
+ *
+ * Return: no return.
+ */
+
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
