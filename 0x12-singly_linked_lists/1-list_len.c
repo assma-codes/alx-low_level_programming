eeedecf3d18090dad8d90331199406a49007d5b2
assma-codes <assma.hm89@gmail.com>
2023-10-20 11:00:27 -0700

    task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *list_len - function calculate no of elements in a list
+ *@h: poiter to the first node
+ *Return: number of elements in a list
+ */
+ 
+size_t list_len(const list_t *h)
+{
+	size_t no_of_elements = 0;
+	if (h == NULL)
+		return (-1);
+	while (h != NULL)
+	{
+		no_of_elements++;
+		h = h->next;
+	}
+	return (no_of_elements);
+}
