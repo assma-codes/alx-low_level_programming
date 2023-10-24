3bcc5aa9dfef5ca45368a12af3ba2b799b3fa4ae
assma-codes <assma.hm89@gmail.com>
2023-10-24 02:03:22 -0700

    task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * size_t print_listint - function prints elements of singe list
+ * @h: pointer of the first node
+ * Return: Number of nodes in the list
+ *
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
