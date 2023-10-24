a6e150d4893c4caa886b7d2cd34487fd51e79953
assma-codes <assma.hm89@gmail.com>
2023-10-24 05:32:41 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint2 - function frees list
+ *@head: pointer to the first element
+ *Return: no return value
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	head = NULL;
+}
