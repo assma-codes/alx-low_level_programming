f6d6f039b83e273b30ea71d3f6dcc0baefad7ee3
assma-codes <assma.hm89@gmail.com>
2023-10-24 03:12:09 -0700

    task2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * add_nodeint - function that adds node to the begining of the list
+ * @head: pointer of the pointer of the first element
+ * @n: int data in the new node
+ * Return: struct new node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode = malloc(sizeof(listint_t));
+
+	if (*head == 0 || newnode == 0)
+		return (NULL);
+	newnode->next = NULL;
+	newnode->n = n;
+	if (*head)
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
+
+}
