b348d2d57ece2446681dd83e598e53f645028e6f
assma-codes <assma.hm89@gmail.com>
2023-10-27 23:36:40 -0700

    test

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,7 +4,7 @@
  * clear_bit - function that sets a bit to 0 at given index
  * @n: The given number
  * @index: The index of which bit to be set
- * Return: 1 on success or -1 on error
+ * Return: 1 on success, -1 on error
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
