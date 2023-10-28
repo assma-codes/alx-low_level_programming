430fdfa6338e7bbeee9b4a2419e6a5d781048177
assma-codes <assma.hm89@gmail.com>
2023-10-27 21:15:02 -0700

    correct code

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,14 +10,13 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int counter = 0;
-	unsigned long int xor = (n ^ m);
-	unsigned long int max = 0x01;
+	unsigned long xor_result = (n ^ m);
+	/*unsigned long int max = 0x01;*/
 
-	while (max <= xor)
+	while (xor_result != 0)
 	{
-		if (max & xor)
-			counter++;
-		max <<= 1;
+		xor_result = xor_result & (xor_result - 1);
+		counter++;
 	}
 	return (counter);
 }
