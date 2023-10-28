a6b7fa18ac405da4b133ee8d193e01ea356b01ce
assma-codes <assma.hm89@gmail.com>
2023-10-27 23:23:04 -0700

    fix wrong output

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,11 +8,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max = ~(max << index);
-	if (max == 0x00)
-		return (-1);
-	*n &= max;
-	return (1);
+	unsigned long int tester = ~(1UL << index);
+	*n = *n & tester;
+	return (*n);
 }
