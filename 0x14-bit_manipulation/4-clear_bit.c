c54aeae1d72e1f3eef8d2a061fd105789a7e54f0
assma-codes <assma.hm89@gmail.com>
2023-10-28 18:33:53 -0700

    fix error

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int tester = ~(1UL << index);
 
-	if (!*n || index > 64)
+	if (!*n || (index >= sizeof(unsigned long int) * 8))
 		return (-1);
 	while (*n && index < 64)
 	{
