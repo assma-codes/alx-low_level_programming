e31022877c294639c8c74ba30b3718090da82375
assma-codes <assma.hm89@gmail.com>
2023-10-28 18:14:23 -0700

    task3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - function that sets a bit to 0 at given index
+ * @n: The given number
+ * @index: The index of which bit to be set
+ * Return: 1 on success, -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int tester = (1UL << index);
+
+	if (!*n || index > 64)
+		return (-1);
+	else
+	{
+		*n |= tester;
+		return (1);
+	}
+	return (*n);
+}
